099fb4cdb06a65691d16565398a4f40a9b9c98ce
roshan.prajapati <roshanprajapati678@gmail.com>
2023-09-30 23:56:08 +0530

    OK

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const dotenv = require('dotenv');
 dotenv.config({path:'./config.env'});
 const cors = require('cors');
 const cookieParser = require("cookie-parser");
-const path = require('path');
+// const path = require('path');
 
 const app = express();
 app.use(cors({
@@ -15,10 +15,10 @@ app.use(express.json({limit: '50mb'}));
 app.use(express.urlencoded({extended:true,limit: '50mb'}));
 
 //static
-app.use(express.static(path.join(__dirname,'./client/dist')));
-app.get("*",function(req,res){
-    res.sendFile(path.join(__dirname,'./client/dist/index.html'));
-})
+// app.use(express.static(path.join(__dirname,'./client/dist')));
+// app.get("*",function(req,res){
+//     res.sendFile(path.join(__dirname,'./client/dist/index.html'));
+// })
 
 require('./src/Database/Connection');
 const router = require('./src/Router/route');
